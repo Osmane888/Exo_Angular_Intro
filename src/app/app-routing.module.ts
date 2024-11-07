845cff165667163e7d9923b06c0a6253d7d2e9be
Guesmia Osmane <osmane688@hotmail.com>
2024-11-07 15:56:54 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'exos', loadChildren: () => import('./exos/exos.module').then(m => m.ExosModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
